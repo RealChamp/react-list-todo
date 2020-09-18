c0a3601129bc08c0b8445fd13e8238487709329a
Matvey <matvey.w2015@mail.ru>
2020-09-19 00:45:24 +0400

    add tasks and checkbox components

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Sidebar } from './Sidebar'
+import {Tasks} from '../Tasks.jsx'
 
 export const Content = () => {
     return (
         <section>
         <Sidebar/>
+        <Tasks/>
     </section>
     )
 }
